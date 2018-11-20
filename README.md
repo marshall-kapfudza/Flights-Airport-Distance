aae7e375efea18c92fd07e88702dc2fed259a36f
Marshall Kapfudza <marshallkapfudza@Marshalls-MBP.fios-router.home>
2018-11-20 10:45:52 -0500

    Flights Airport Distance

---
+++ README.md
@@ -0,0 +1,11 @@
+# Flights Airport Distance
+
+### Overview
+ 
+Design and implement a single-page application that calculates and displays, given user input, the distance between two airports.
+
+### Requirements
+- A simple, clean design that puts easy user interaction first
+- Flights only between the following airports - JFK, LAX, LAS, PDX
+- Well-written Javascript, HTML, and CSS
+- Use of jQuery or raw DOM functions is acceptable, but please avoid usage of any other front-end frameworks.
---
+++ src/flights.js
@@ -0,0 +1,68 @@
+var IntentMedia = IntentMedia || {};
+
+IntentMedia.Airports = (function () {
+  const pub = {};
+
+  pub.airport_exists = function (airport_code) {
+    return pub.airport_distances().hasOwnProperty(airport_code);
+  };
+
+  pub.airport_distances = function () {
+    const data = {
+      JFK: {
+        LAX: 2475,
+        LAS: 2248,
+        PDX: 2454,
+      },
+      LAX: {
+        JFK: 2475,
+        LAS: 236,
+        PDX: 834,
+      },
+      LAS: {
+        JFK: 2248,
+        LAX: 236,
+        PDX: 763,
+      },
+      PDX: {
+        JFK: 2454,
+        LAS: 763,
+        LAX: 834,
+      },
+    };
+    return data;
+  };
+  return pub;
+}(IntentMedia || {}));
+
+IntentMedia.Distances = (function () {
+  const pub = {};
+  var airport_distances = airport_distances || IntentMedia.Airports.airport_distances();
+
+  pub.distance_between_airports = function (from_airport, to_airport) {
+    if (IntentMedia.Airports.airport_exists(from_airport) && IntentMedia.Airports.airport_exists(to_airport)) {
+      if (from_airport === to_airport) {
+        return 0;
+      }
+      return airport_distances[from_airport][to_airport];
+    }
+    return -1;
+  };
+
+  return pub;
+}(IntentMedia || {}));
+
+function distBetweenAirports() {
+  const a = document.getElementById('pointA').value.toUpperCase();
+  const b = document.getElementById('pointB').value.toUpperCase();
+  const svg = document.getElementById('plane');
+  const value = IntentMedia.Distances.distance_between_airports(a, b);
+  
+  if(value != -1) {
+    svg.classList.add('success');
+    document.getElementById('output').innerHTML = `<p>The distance between <b>${a}</b> to <b>${b}</b> is <b>${value} Miles</b></p>`;
+  } else {
+    svg.classList.remove('success');
+    document.getElementById('output').innerHTML = `<p class="error">Oops! Airpot distance from <b>${a}</b> to <b>${b}</b> does not exist</p>`;
+  }
+}
